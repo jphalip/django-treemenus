0b36472845b7efebbe2d38ec14714daf3e578574
Julien Phalip <jphalip@gmail.com>
2010-01-21 03:03:58 +0000

    Updated setup.py - Now ready to release 0.8.5

--- setup.py
+++ setup.py
@@ -1,42 +1,21 @@
-from distutils.core import setup
-import os
+from setuptools import setup, find_packages
 
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk('treemenus'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[10:] # Strip "treemenus/" or "treemenus\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
-
-
-setup(name='treemenus',
-      version='0.0',
-      description='Tree-like menuing application for Django',
-      author='Julien Phalip',
-      author_email='jipedev@gmail.com',
-      url='http://code.google.com/p/django-treemenus/',
-      package_dir={'treemenus': 'treemenus'},
-      packages=packages,
-      package_data={'treemenus': data_files},
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'],
-      )
+setup(
+    name='django-treemenus',
+    version='0.8.5',
+    description='Tree-like menuing application for Django.',
+    author='Julien Phalip',
+    author_email='julien@julienphalip.com',
+    url='http://code.google.com/p/django-treemenus/',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
