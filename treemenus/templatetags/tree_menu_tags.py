186e4c71b647533083ee6bf1c4010806798c5a8c
artscoop <artscoop93.info@gmail.com>
2013-02-21 22:46:20 +0100

    Update treemenus/templatetags/tree_menu_tags.py
    Fix error on Django 1.5 because of deprecated template tag syntax since Django 1.3.

--- treemenus/templatetags/tree_menu_tags.py
+++ treemenus/templatetags/tree_menu_tags.py
@@ -48,7 +48,7 @@ class ReverseNamedURLNode(Node):
         from django.template import TOKEN_BLOCK, Token
 
         resolved_named_url = self.named_url.resolve(context)
-        contents = u'url ' + resolved_named_url
+        contents = u'url "%s"' % (resolved_named_url,)
 
         urlNode = url(self.parser, Token(token_type=TOKEN_BLOCK, contents=contents))
         return urlNode.render(context)
